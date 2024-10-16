c124fc90e98b6a8cc8a69e19934dcb58e4dec9da
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-10-16 18:43:05 +0300

    RA-03-create-project 1) Подключен ViewBinding 2)В методе onCreate() инициализируется binding и передается в setContentView 3) В основной контейнер добавлен контейнер для кнопок с настройками отступа. Значения отступов берутся из ресурсного фай 4) В ресурсы добавлена иконка для кнопки "Избранное" 5) В ресурсы добавлен шрифт montserrat_alternates_semibold 6) Реализованы кнопки с настройкой стиля кнопок и текста из ресурсных файлов стилей. 7) Для StatusBar и NavigationBar фон сделан как и основной

--- app/src/main/java/com/example/recipesapp/MainActivity.kt
+++ app/src/main/java/com/example/recipesapp/MainActivity.kt
@@ -3,18 +3,18 @@ package com.example.recipesapp
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.recipesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding
+        get() = _binding
+            ?: throw IllegalStateException("binding for ActivityMainBinding must not be null ")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(binding.root)
+
     }
 }
