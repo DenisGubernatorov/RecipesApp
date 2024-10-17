ac5ba89e1520904d8a16fdccae0154c2baf6c9aa
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-10-18 00:27:44 +0300

    RA-03-create-project 1) com.example.recipesapp.MainActivity.getBinding переведен на ленивую инициализацию 2) Абсолютные значения в атрибутах элементов переведены на использование ресурсных файлов

--- app/src/main/java/com/example/recipesapp/MainActivity.kt
+++ app/src/main/java/com/example/recipesapp/MainActivity.kt
@@ -1,20 +1,16 @@
 package com.example.recipesapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.recipesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private var _binding: ActivityMainBinding? = null
-    private val binding
-        get() = _binding
-            ?: throw IllegalStateException("binding for ActivityMainBinding must not be null ")
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        _binding = ActivityMainBinding.inflate(layoutInflater)
-        enableEdgeToEdge()
         setContentView(binding.root)
-
     }
 }
