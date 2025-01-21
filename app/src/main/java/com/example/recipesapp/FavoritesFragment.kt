ff16ad9b4a7304bcc65fbdfd4c15f33981c07b17
Denis Gubernatorov <dygubernatorov@gmail.com>
2025-01-21 20:16:46 +0300

    RA-18-Favorites-Screen 1) Реализация лэйаута для экрана избранного 2) Пока показывает заглушку на экране

--- app/src/main/java/com/example/recipesapp/FavoritesFragment.kt
+++ app/src/main/java/com/example/recipesapp/FavoritesFragment.kt
@@ -1,11 +1,14 @@
 package com.example.recipesapp
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.recipesapp.databinding.FragmentFavoritesBinding
+import java.io.IOException
 
 
 class FavoritesFragment : Fragment() {
@@ -20,6 +23,21 @@ class FavoritesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentFavoritesBinding.inflate(inflater)
+
+        binding.favoritesListHeaderImg.setImageDrawable(
+            try {
+                binding.root.context.assets.open("bcg_favorites.png")
+                    .use { inputStream ->
+                        Drawable.createFromStream(inputStream, null)
+                    }
+            } catch (e: IOException) {
+                Log.e("!!!!__", "image  for favorites not found ", e)
+                null
+            })
+
+        binding.emptyRecipesList.visibility = View.VISIBLE
+        binding.rvRecipes.visibility = View.GONE
+
         return binding.root
     }
 
