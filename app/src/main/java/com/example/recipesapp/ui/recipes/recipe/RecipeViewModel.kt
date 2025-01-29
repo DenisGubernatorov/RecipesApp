32ab665c3f611875f43fa4571eb80be8ed036a0c
Denis Gubernatorov <dygubernatorov@gmail.com>
2025-01-30 01:23:27 +0300

    RA-22-LiveData-Realisation 1) Добавление логики работы с LiveData

--- app/src/main/java/com/example/recipesapp/ui/recipes/recipe/RecipeViewModel.kt
+++ app/src/main/java/com/example/recipesapp/ui/recipes/recipe/RecipeViewModel.kt
@@ -1,9 +1,19 @@
 package com.example.recipesapp.ui.recipes.recipe
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.recipesapp.model.Recipe
 
-class RecipeViewModel() : ViewModel() {
+class RecipeViewModel : ViewModel() {
+    private var _rfLiveData: MutableLiveData<RecipeViewModelState> = MutableLiveData()
+    val rfLiveData: LiveData<RecipeViewModelState> get() = _rfLiveData
+
+    init {
+        Log.i("RST", "recipe model created")
+        _rfLiveData.value = RecipeViewModelState(isFavorite = true)
+    }
 
 }
 
