316cde4ffe827d084e860eca0a605803e8383002
Denis Gubernatorov <dygubernatorov@gmail.com>
2025-02-27 12:00:58 +0300

    RA-32-Query-By-UrlConnection Работа с сетью 1) Данные для списка категорий получаются через url запрос. 2) Добавлена зависимость для сериализации json 3) В лог выводится в каких потоках работаем, что получили с сервера, как десериализовали

--- app/src/main/java/com/example/recipesapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/recipesapp/ui/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.example.recipesapp.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import com.example.recipesapp.R
 import com.example.recipesapp.databinding.ActivityMainBinding
+import com.example.recipesapp.model.Category
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +21,36 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        Log.i("thread", "Метод onCreate() выполняется на потоке: ${Thread.currentThread().name}")
+
+        val thread = Thread {
+            val url = URL("https://recipes.androidsprint.ru/api/category")
+            val connection = url.openConnection() as HttpURLConnection
+            try {
+                connection.connect()
+                Log.i("thread", "Выполняю запрос на потоке: ${Thread.currentThread().name}")
+
+                val bufferedData =
+                    connection.inputStream.bufferedReader().use { reader -> reader.readText() }
+                Log.i("data", bufferedData)
+
+                val json = Json { ignoreUnknownKeys = true }
+                val categories: List<Category> = json.decodeFromString(bufferedData)
+                categories.forEach { category ->
+                    Log.i(
+                        "category",
+                        "ID: ${category.id}, Name: ${category.title}, description: ${category.description}, imageURL: ${category.imageUrl}"
+                    )
+                }
+            } catch (e: Exception) {
+                Log.e("connection", e.toString())
+            } finally {
+                connection.disconnect()
+            }
+        }
+
+        thread.start()
 
         binding.category.setOnClickListener {
             val navController = findNavController(R.id.mainContainer)
